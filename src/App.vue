c4d62dd1da01df69dc0177390ff78987d55631d4
Svrch <sapeshin@yandex.ru>
2024-06-13 18:39:42 +0500

    four

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { computed } from 'vue'
 import { useAuthStore } from './stores/auth';
 
 const authStore = useAuthStore()
+
+const token = computed(() => authStore.userInfo.token)
 
 const checkUser = () => {
   const tokens = JSON.parse(localStorage.getItem('userTokens'))
@@ -16,6 +19,17 @@ checkUser()
 </script>
 
 <template>
+  <div class="menu">
+    <router-link to="/">Home</router-link>
+    <router-link
+      to="/signin"
+      v-if="!token"
+    >Sign In</router-link>
+    <router-link
+      to="/cars"
+      v-if="token"
+    >Cars</router-link>
+  </div>
   <div class="container">
     <RouterView />
   </div>
@@ -26,5 +40,18 @@ checkUser()
   max-width: 700px;
   margin: auto;
   font-family: Arial, sans-serif;
+}
+
+.menu {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 20px;
+  font-size: 20px;
+}
+
+.menu__link {
+  color: #000;
+  margin: 0 20px;
+  font-family: 'Arial', sans-serif;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from '../views/HomeView.vue'
 import SignUp from '../views/SignUp.vue'
 import SignIn from '../views/SignIn.vue'
 import Cars from '@/views/Cars.vue'
+import { useAuthStore } from '@/stores/auth'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,24 +12,46 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: HomeView,
+      meta: {
+        auth: false
+      }
     },
     {
       path: '/signup',
       name: 'signup',
-      component: SignUp
+      component: SignUp,
+      meta: {
+        auth: false
+      }
     },
     {
       path: '/signin',
       name: 'signin',
-      component: SignIn
+      component: SignIn,
+      meta: {
+        auth: false
+      }
     },
     {
       path: '/cars',
       name: 'cars',
-      component: Cars
+      component: Cars,
+      meta: {
+        auth: true
+      }
     },
   ]
 })
 
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore()
+  if (to.meta.auth && !authStore.userInfo.token) {
+    next('/signin')
+  } else if (!to.meta.auth && authStore.userInfo.token) {
+    next('/cars')
+  } else {
+    next()
+  }
+})
 export default router
